2315ab6b205cc16c2bd64b8aeb4f9807d2029d9d
Geoffrey Métais <geoffrey.metais@gmail.com>
2014-10-09 12:40:37 +0200

    Basic Audio player for Android TV

---
+++ vlc-android/src/org/videolan/vlc/gui/tv/audioplayer/PlaylistAdapter.java
@@ -0,0 +1,53 @@
+package org.videolan.vlc.gui.tv.audioplayer;
+
+import java.util.ArrayList;
+
+import org.videolan.libvlc.Media;
+import org.videolan.vlc.MediaLibrary;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class PlaylistAdapter extends RecyclerView.Adapter<PlaylistAdapter.ViewHolder> {
+    private ArrayList<String> mDataset;
+	private static MediaLibrary sMediaLibrary = MediaLibrary.getInstance();
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTitleTv;
+        public TextView mArtistTv;
+        public ViewHolder(View v) {
+            super(v);
+            mTitleTv = (TextView) v.findViewById(android.R.id.text1);
+            mArtistTv = (TextView) v.findViewById(android.R.id.text2);
+        }
+    }
+
+    public PlaylistAdapter(ArrayList<String> myDataset) {
+        mDataset = myDataset;
+    }
+
+    @Override
+    public PlaylistAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                               .inflate(android.R.layout.simple_list_item_2, parent, false);
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Media media = sMediaLibrary.getMediaItem(mDataset.get(position));
+        holder.mTitleTv.setText(media.getTitle());
+        holder.mArtistTv.setText(media.getArtist());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
